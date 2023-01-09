9d8f19e53c3329bf29475d8de9d41236e69097d8
Smiley98 <smileysofficialcinema@gmail.com>
2023-01-09 15:20:32 -0500

    Condition lesson

--- main.cpp
+++ main.cpp
@@ -2,13 +2,46 @@
 
 using namespace std;
 
+
+void function()
+{
+	printf("I AM A FUNCTION!!!!\n");
+}
+
 int main()
 {
-	int number = 5;
-	int* numberPointer = nullptr;
-	if (numberPointer == nullptr)
-		numberPointer = &number;
-	printf("%i\n", *numberPointer);
+	int number = 7;
+	//int* numberPointer = nullptr;
+	//if (numberPointer == nullptr)
+	//	numberPointer = &number;
+	//printf("%i\n", *numberPointer);
+
+	// if-statements only evaluate booleans
+	// It's best to only pass booleans. However,
+	// 0 evaluates to false and anything non-zero evaluates to true:
+	//int condition = 9023485023945098; <-- true
+	//int falseCondition = 0 <-- false
+	//bool condition = number == 6;
+	//if (condition)
+	//	printf("True!\n");
+	//else
+	//	printf("False\n");
+
+	// In if-blocks, only the true statement is executed.
+	// Statements are evaluated sequentially (top-down).
+	if (number == 4)
+		printf("4\n");
+	else if (number == 5)
+		printf("5\n");
+	else if (number == 6)
+		printf("6\n");
+	else if (number == 7)
+		printf("7\n");
+	else if (number == 8)
+		printf("8\n");
+	else
+		printf("Your number is not 4 or 5\n");
+
 	// Just like normal variables, pointers should be default-initialized.
 	// Otherwise, you get corrupted memory
 	//int goodNumber = 5;
@@ -26,5 +59,7 @@ int main()
 
 	//printf("%i %i %i %i\n", a, b, c, d);
 	//printf("%i %i %i %i\n", *arr[0], *arr[1], *arr[2], *arr[3]);
+	
+	cin.get();
 	return 0;
 }
