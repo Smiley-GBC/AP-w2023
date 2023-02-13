fdbee7b40025b6c5904fbf2290d17aaa07d3dc33
Smiley98 <smileysofficialcinema@gmail.com>
2023-02-13 14:35:17 -0500

    Static array finished

--- lab6.cpp
+++ lab6.cpp
@@ -1,4 +1,6 @@
 #include <cassert>
+#include <string>
+using namespace std;
 
 template<typename T, size_t size>
 class Array
@@ -21,3 +23,9 @@ int main()
 	integers[15] = 2;
 	return 0;
 }
+
+// "using" allows us to give a type an alias. The following lines are identical to the compiler.
+//using Number = int;
+//int a;
+//Number a;
+// Note to self, the STL is a dark dark place
