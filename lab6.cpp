51a9f2d900d6be0763eebb3458f70a71cfb8c1e3
Smiley98 <smileysofficialcinema@gmail.com>
2023-02-13 14:16:03 -0500

    Added int and float arrays

--- lab6.cpp
+++ lab6.cpp
@@ -1,4 +1,37 @@
+#include <cassert>
+
+class DecimalArray
+{
+public:
+	float& operator[](size_t index)
+	{
+		assert(index < mSize);
+		return mData[index];
+	}
+
+private:
+	float mData[16];
+	const size_t mSize = 16;
+};
+
+class IntegerArray
+{
+public:
+	float& operator[](size_t index)
+	{
+		assert(index < mSize);
+		return mData[index];
+	}
+
+private:
+	float mData[16];
+	const size_t mSize = 16;
+};
+
 int main()
 {
+	DecimalArray decimals;
+	IntegerArray integers;
+	integers[16] = 2;
 	return 0;
 }
